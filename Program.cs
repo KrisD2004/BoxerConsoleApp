2e7062ceb658ad04c66551a0156c0fc87dfa5813
Kristian Dotson <KDotson387@gmail.com>
2023-09-18 09:58:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReviewWeekApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
